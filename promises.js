7b86552354556d1c8456640a11a9c235e9650539
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-22 15:06:35 +0200

    added rout's

--- promises.js
+++ promises.js
@@ -1,15 +1,45 @@
-import fs from 'fs';
+import fs from "fs";
 
-const data =
-new Promise((res, rej) => {
-fs.readFile('data/targets.json', 'utf-8', (err, data) => {
-  if(err){
-    console.log(err)
-    throw err
-  }
-  res(data)
-})
-}).catch(err => {
-  console.log(err)
-})
-console.log(await data)
+// const data =
+// new Promise((res, rej) => {
+// fs.readFile('data/targets.json', 'utf-8', (err, data) => {
+//   if(err){
+//     console.log(err)
+//     throw err
+//   }
+//   res(data)
+// })
+// }).catch(err => {
+//   console.log(err)
+// })
+// console.log(await data)
+
+// function sliceObject(obj, keys) {
+//   return Object.fromEntries(
+//     Object.entries(obj).filter(([key, value]) => keys.includes(key))
+//   );
+// }
+
+// const myObj = {
+//   id: "t-102",
+//   codeName: "Iron Falcon",
+//   region: "north",
+//   priority: 4,
+//   status: "new",
+//   createdAt: "2025-12-21T09:30:00.000Z",
+// };
+
+// console.log(sliceObject(myObj, ["id", "codeName"]));
+
+// function sliceObject(obj, ...keys) {
+//   const data = ({ ...keys } = obj);
+//   return { ...data };
+// }
+// const obj = {
+//   name: "efraim",
+//   age: 21,
+//   city: "modiin",
+// };
+
+// console.log(sliceObject(obj, "name", "age"));
+console.log(new Date)
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { time } from "console";
 import express from "express";
+import { lchown } from "node:fs";
 import fs from "node:fs/promises";
+import file from "./utils/fileHandling.js";
 
 const app = express();
 const port = 3000;
@@ -8,7 +10,14 @@ const port = 3000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`req: [${req.method} - ${req.url}]`);
+  res.set({
+    "X-Server-Start-Time": new Date(),
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`req: [${req.method} - ${req.url}] | timwstamp: ${new Date()}`);
   next();
 });
 
@@ -20,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  const timeStamp = Date.now();
+  const timeStamp = new Date();
   res.json({ status: "ok", serverTime: timeStamp });
   console.log("health check");
 });
@@ -48,7 +57,65 @@ app.get("/targets/:id", async (req, res) => {
   }
   res.json(result);
 });
+
+app.get("/targets", async (req, res) => {
+  const region = req.query.region;
+  const status = req.query.status;
+  const minPriority = req.query.minPriority;
+
+  if (!(region && status && minPriority)) {
+    res.status(404).send("params error !");
+  }
+  const targets = JSON.parse(
+    await fs.readFile(process.cwd() + "/data/targets.json", "utf-8")
+  ).targets;
+  const result = targets.filter(
+    (t) =>
+      t.region === region && t.status === status && t.priority >= minPriority
+  );
+  if (result.length == 0) {
+    res.send("not found !");
+    return;
+  }
+  res.json(result);
+});
+
+app.post("/targets", async (req, res) => {
+  const targets = JSON.parse(
+    await fs.readFile(process.cwd() + "/data/targets.json", "utf-8")
+  ).targets;
+  const target = {
+    id: Math.random()
+      .toString(36)
+      .substring(2, 6 + 2),
+    createdAt: new Date(),
+  };
+  Object.assign(target, req.body);
+  targets.push(target);
+  await fs.writeFile(
+    process.cwd() + "/data/targets.json",
+    JSON.stringify({ targets }, null, 2)
+  );
+  res.status(201).json({ sucsess: target });
+});
+
+app.post("/targets/:id", async (req, res) => {
+  const targets = JSON.parse(
+    await fs.readFile(process.cwd() + "/data/targets.json", "utf-8")
+  ).targets;
+  const result = targets.findIndex((target) => target.id === req.params.id);
+  if (!result) {
+    res.status(404).json({ message: `${req.params.id} not found` });
+  }
+  const updatedTarget = Object.assign(targets[result], req.body);
+  targets[result] = updatedTarget;
+  await fs.writeFile(
+    process.cwd() + "/data/targets.json",
+    JSON.stringify({ targets }, null, 2)
+  );
+  res.status(201).json({ sucsess: targets[result] });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
-
